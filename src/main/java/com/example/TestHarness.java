6bb80dd8298cd2217135ac0f0c69692615d9e71a
Vinay A <vinay.a@smnetserv.com>
2025-05-30 21:15:29 +0530

    latest code

--- src/main/java/com/example/TestHarness.java
+++ src/main/java/com/example/TestHarness.java
@@ -6,18 +6,23 @@ import org.testng.annotations.Test;
 public class TestHarness {
 	@Test
 	public static void main(String[] args) {
-		String tc = System.getenv("TEST_CASE");
+        String tc = System.getenv("TEST_CASE");
+        TestNG testng = new TestNG();
 
-		TestNG testng = new TestNG();
-		if ("tc001".equals(tc)) {
-			testng.setTestClasses(new Class[] { TC001.class });
-		} else if ("tc002".equals(tc)) {
-			testng.setTestClasses(new Class[] { TC002.class });
-		} else if ("tc003".equals(tc)) {
-			testng.setTestClasses(new Class[] { TC003.class });
-		} else {
-			testng.setTestClasses(new Class[] { TC001.class, TC002.class, TC003.class });
-		}
-		testng.run();
-	}
+        switch (tc) {
+            case "tc001":
+                testng.setTestClasses(new Class[]{TC001.class});
+                break;
+            case "tc002":
+                testng.setTestClasses(new Class[]{TC002.class});
+                break;
+            case "tc003":
+                testng.setTestClasses(new Class[]{TC003.class});
+                break;
+            default:
+                testng.setTestClasses(new Class[]{TC001.class, TC002.class, TC003.class});
+                break;
+        }
+        testng.run();
+    }
 }
